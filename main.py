00effb6119f986ad8102649830f06bf340e0ffc6
zacbox <60763465+ZacWorksMOE@users.noreply.github.com>
2024-05-13 23:30:25 +0800

    002 Commit

--- main.py
+++ main.py
@@ -3,15 +3,23 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
+from datetime import datetime  # https://www.w3schools.com/python/python_datetime.asp
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-    print(f'Bye, {name}')
+    if not hasattr(print_hi, "counter"):
+        print_hi.counter = 0  # it doesn't exist yet, so initialize it
+    print_hi.counter += 1
+
+    # datetime object containing current date and time
+    now = datetime.now()
+    print(f'{print_hi.counter:3} {now} Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    print(f'{print_hi.counter:3} {now} Bye, {name}')
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print_hi("Linux")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
